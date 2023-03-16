9fded9ce69384a45102b0fe923babfca909a182d
VitaliyCeban <vitaly.ceban@gmail.com>
2023-03-16 16:14:42 +0200

    Added project Havit.Blazor.SoftLider

---
+++ Havit.Blazor.SoftLider/Components/SlSidebar.razor.cs
@@ -0,0 +1,24 @@
+﻿using Havit.Blazor.Components.Web.Bootstrap;
+using Microsoft.JSInterop;
+
+namespace Havit.Blazor.SoftLider.Components;
+
+public partial class SlSidebar : HxSidebar
+{
+    //protected override async Task OnInitializedAsync()
+    //{
+    //    //await JSRuntime.InvokeVoidAsync("eval", $$"""
+    //    //    var config = {
+    //    //        elementID: 'touchSideSwipe',
+    //    //        elementWidth: 400, //px
+    //    //        elementMaxWidth: 0.8, // *100%
+    //    //        sideHookWidth: 44, //px
+    //    //        moveSpeed: 0.2, //sec
+    //    //        opacityBackground: 0.8,
+    //    //        shiftForStart: 50, // px
+    //    //        windowMaxWidth: 992, // px
+    //    //    }
+    //    //    var touchSideSwipe = new TouchSideSwipe(config);
+    //    //    """);
+    //}
+}
---
+++ Havit.Blazor.SoftLider/UISettings.cs
@@ -0,0 +1,95 @@
+﻿using Havit.Blazor.Components.Web;
+using Havit.Blazor.Components.Web.Bootstrap;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Havit.Blazor.SoftLider;
+
+public class UISettings
+{
+    public static IServiceCollection Initialize(IServiceCollection services)
+    {
+        services.AddHxServices();
+        services.AddHxMessenger();
+        services.AddHxMessageBoxHost();
+        SetDefaults();
+        return services;
+    }
+
+    public static ModalSettings FormSettings { get; } = new()
+    {
+        Backdrop = ModalBackdrop.Static,
+        Scrollable = true,
+        Size = ModalSize.Large,
+        Fullscreen = ModalFullscreen.LargeDown,
+        ContentCssClass = "vh-100"
+    };
+
+    public static ModalSettings FullscreenFormSettings { get; } = new()
+    {
+        Backdrop = ModalBackdrop.False,
+        Scrollable = true,
+        Fullscreen = ModalFullscreen.Always,
+        ContentCssClass = "vh-100"
+    };
+
+    public static ButtonSettings AddButtonSettings { get; } = new()
+    {
+        Color = ThemeColor.Primary,
+        Icon = BootstrapIcon.PlusLg,
+    };
+
+    public static ButtonSettings SelectButtonSettings { get; } = new()
+    {
+        Color = ThemeColor.Primary,
+        Icon = BootstrapIcon.Check2,
+    };
+
+    public static ButtonSettings RefreshButtonSettings { get; } = new()
+    {
+        TextCssClass = "d-none d-sm-inline",
+        Color = ThemeColor.Light,
+        Icon = BootstrapIcon.ArrowClockwise
+    };
+
+    private static void SetDefaults()
+    {
+        HxAutosuggest.Defaults.MinimumLength = 1;
+        HxSearchBox.Defaults.MinimumLength = 1;
+
+        HxButton.Defaults.Size = ButtonSize.Small;
+        HxButton.Defaults.Spinner = false;
+
+        HxInputText.Defaults.InputSize = InputSize.Small;
+        HxInputNumber.Defaults.InputSize = InputSize.Small;
+        HxInputDate.Defaults.InputSize = InputSize.Small;
+        HxInputDateRange.Defaults.InputSize = InputSize.Small;
+        HxInputFile.Defaults.InputSize = InputSize.Small;
+        HxSelect.Defaults.InputSize = InputSize.Small;
+        HxMultiSelect.Defaults.InputSize = InputSize.Small;
+        HxInputTags.Defaults.InputSize = InputSize.Small;
+        HxSearchBox.Defaults.InputSize = InputSize.Small;
+        HxFormValue.Defaults.InputSize = InputSize.Small;
+        HxAutosuggest.Defaults.InputSize = InputSize.Small;
+
+        HxModal.Defaults.HeaderCssClass = "gap-2";
+        HxModal.Defaults.Centered = true;
+
+        HxPlaceholderContainer.Defaults.CssClass = "text-start";
+        HxPlaceholder.Defaults.Color = ThemeColor.Light;
+        HxPlaceholder.Defaults.CssClass = "rounded";
+
+        HxGrid.Defaults.Responsive = true;
+        HxGrid.Defaults.ContentNavigationMode = GridContentNavigationMode.InfiniteScroll;
+        HxGrid.Defaults.OverscanCount = 10;
+        HxGrid.Defaults.TableCssClass = "flex-grow-1 table-layout-fixed position-absolute";
+        HxGrid.Defaults.TableContainerCssClass = "flex-grow-1 d-flex flex-column position-relative";
+
+        HxInputBase.Defaults.ValidationMessageMode = ValidationMessageMode.Regular;
+
+        HxFormValue.Defaults.ValueCssClass = "grayed";
+
+        HxListLayout.Defaults.CssClass = "flex-grow-1 d-flex flex-column";
+        HxListLayout.Defaults.CardSettings.CssClass = "flex-grow-1";
+        HxListLayout.Defaults.CardSettings.BodyCssClass = "flex-grow-0";
+    }
+}
