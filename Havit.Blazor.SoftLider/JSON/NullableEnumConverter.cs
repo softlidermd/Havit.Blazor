c54ff314de36a29b75eb405e6a63d54b9c027893
VitaliyCeban <vitaly.ceban@gmail.com>
2023-12-26 11:01:32 +0200

    .

---
+++ Havit.Blazor.SoftLider/JSON/NullableEnumConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Havit.Blazor.SoftLider.JSON;
+
+public class NullableEnumConverter<TEnum> : JsonConverter<TEnum?> where TEnum : struct, Enum
+{
+	public override TEnum? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		if (reader.TokenType == JsonTokenType.Null)
+		{
+			return null;
+		}
+
+		string enumString = reader.GetString();
+		if (Enum.TryParse(enumString, true, out TEnum result))
+		{
+			return result;
+		}
+
+		return null;
+	}
+
+	public override void Write(Utf8JsonWriter writer, TEnum? value, JsonSerializerOptions options)
+	{
+		if (value.HasValue)
+		{
+			writer.WriteStringValue(value.ToString());
+		}
+		else
+		{
+			writer.WriteNullValue();
+		}
+	}
+}
